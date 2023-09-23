cc4dabb9a8a52b550b47b92b4f55c57ea6e14d3d
AG4lyf <surajbhari159@gmail.com>
2023-09-24 00:08:12 +0530

    streak command

--- api.py
+++ api.py
@@ -4,7 +4,6 @@ import sqlite3
 from datetime import datetime, timedelta
 from urllib.parse import parse_qs
 import requests
-import builtins
 import asyncio
 import time
 
@@ -21,6 +20,7 @@ def main():
 
 @app.get("/stats")
 def stats():
+    tt = time.time()
     try:
         channel = parse_qs(request.headers["Nightbot-Channel"])
         user = parse_qs(request.headers["Nightbot-User"])
@@ -36,33 +36,31 @@ def stats():
     user_id = user.get("providerId")[0]
     # user_id = "UCPgQs00LJYcATD0Uf7naPwA"
     user_name = user.get("displayName")[0]
-    try:
-        return " "
-    finally:
-        ranking_data = get_ranking(channel_id)
-        oldest_data = get_oldest(channel_id)
-        ranking = 0
-        for x in ranking_data:
-            ranking += 1
-            if x[0] == user_id:
-                break
-        old_ranking = 0
-        for y in oldest_data:
-            old_ranking += 1
-            if y[0] == user_id:
-                first_message = y
-                break
-        total_count = len(oldest_data)
-        first_stream_dt = datetime.fromtimestamp(int(float(first_message[3])))
-        ago = datetime.now() - first_stream_dt
-        requests.post(response_url, data={"message": f"{user_name} is ranked #{ranking} in chat with {ranking_data[ranking-1][4]} messages. Their first message was on stream that was streamed {ago.days} days ago. and {old_ranking}/{total_count} member to this cult."})
+    ranking_data = get_ranking(channel_id)
+    oldest_data = get_oldest(channel_id)
+    ranking = 0
+    for x in ranking_data:
+        ranking += 1
+        if x[0] == user_id:
+            break
+
+    old_ranking = 0
+    for y in oldest_data:
+        old_ranking += 1
+        if y[0] == user_id:
+            first_message = y
+            break
+
+    total_count = len(oldest_data)
+    first_stream_dt = datetime.fromtimestamp(int(float(first_message[3])))
+    ago = datetime.now() - first_stream_dt
+    return f"{user_name} is ranked #{ranking} in chat with {ranking_data[ranking-1][4]} messages. Their first message was on stream that was streamed {ago.days} days ago. and {old_ranking}/{total_count} member to this cult."
 
 
 def get_oldest(channel_id: str):
     query = f"SELECT user_id, user_name, stream_id, message_origin_time, MIN(message_origin_time) AS first_message_time, message_content FROM {channel_id} GROUP BY user_id ORDER BY first_message_time ASC;"
     cursor.execute(query)
     data = cursor.fetchall()
-    conn.commit
     return data
 
 
@@ -70,9 +68,35 @@ def get_ranking(channel_id: str):
     query = f"SELECT user_id, user_name, stream_id, message_origin_time, COUNT(*) as num_messages FROM {channel_id} GROUP BY user_id ORDER BY num_messages DESC;"
     cursor.execute(query)
     data = cursor.fetchall()
-    conn.commit()
     return data
 
+@app.get("/streak")
+def streak():
+    try:
+        channel = parse_qs(request.headers["Nightbot-Channel"])
+        response_url = request.headers["Nightbot-Response-Url"]
+        user = parse_qs(request.headers["Nightbot-User"])
+    except KeyError:
+        return "Not able to auth" 
+    user_id = user['providerId'][0]
+    channel_id = channel.get("providerId")[0]
+    # channel_id = "UCIzzPhdRf8Olo3WjiexcZSw"
+    channel_id = channel_id.replace(
+        "-", "_"
+    )  # YT is weird with channel ids. some have - in them. but sql tables cant have - in them. so we replace it with _
+    user_name = user.get("displayName")[0]
+    cursor.execute(f"SELECT DISTINCT stream_id FROM {channel_id}")
+    streams = cursor.fetchall()
+    count = 0
+    cursor.execute(f"SELECT DISTINCT stream_id FROM {channel_id} WHERE user_id = '{user_id}'")
+    data = cursor.fetchall()
+    present_in = len(data)
+    total_streams = len(streams)
+    for i in range(len(streams)):
+        if streams[i] != data[i]:
+            break
+        count += 1
+    return f"@{user_name} {count} streams in a row. You were present in {present_in}/{total_streams} streams."
 
 @app.get("/youngest/<query>")
 @app.get("/youngest/")
@@ -90,30 +114,30 @@ async def youngest(query=None):
         response_url = request.headers["Nightbot-Response-Url"]
     except KeyError:
         return "Not able to auth"
-
+    
     channel_id = channel.get("providerId")[0]
     # channel_id = "UCIzzPhdRf8Olo3WjiexcZSw"
     channel_id = channel_id.replace("-", "_")
-    data = get_oldest(channel_id)
-    string = ""
-    counter = 1
-    x = query * -1 - 1
-    for x in data[x:-1][::-1]:
-        ago = datetime.now() - datetime.fromtimestamp(int(float(x[4])))
-        ago = ago.days * 24 + ago.seconds // 3600
-        string += f"{str(counter)}.{str(x[1])}: {ago} hours ago.  "
-        counter += 1
-    if len(string) > 200:
-        try:
-            return string[:200]
-        finally:
-            await asyncio.sleep(5)
-            parts = [string[i : i + 200] for i in range(200, len(string), 200)]
+    try:
+        return "Sorry, for spam!"
+    finally:
+        data = get_oldest(channel_id)
+        string = ""
+        counter = 1
+        x = query * -1 - 1
+        for x in data[x:-1][::-1]:
+            ago = datetime.now() - datetime.fromtimestamp(int(float(x[4])))
+            ago = ago.days * 24 + ago.seconds // 3600
+            string += f"| {str(counter)}.{str(x[1])}: {ago} hours ago.  "
+            counter += 1
+        if len(string) > 200:
+            parts = [string[i : i + 200] for i in range(0, len(string), 200)]
             for part in parts:
+                await asyncio.sleep(4.5)
+
                 requests.post(response_url, data={"message": part})
-                await asyncio.sleep(5)
-    else:
-        return string
+        else:
+            requests.post(response_url, data={"message": string})
 
 
 @app.get("/oldest/<query>")
@@ -146,17 +170,14 @@ async def oldest(query=None):
         counter += 1
     if len(string) > 200:
         try:
-            return string[:200]
+            "Sorry, for spam!"
         finally:
-            await asyncio.sleep(5)
-            parts = [string[i : i + 200] for i in range(200, len(string), 200)]
+            parts = [string[i : i + 200] for i in range(0, len(string), 200)]
             for part in parts:
                 requests.post(response_url, data={"message": part})
-                await asyncio.sleep(5)
-            return " "
     else:
         return string
-
+     
 
 @app.get("/top/<query>")
 @app.get("/top/")
@@ -174,10 +195,10 @@ async def top(query=None):
         response_url = request.headers["Nightbot-Response-Url"]
     except KeyError:
         return "Not able to auth"
-
     channel_id = channel.get("providerId")[0]
     # channel_id = "UCIzzPhdRf8Olo3WjiexcZSw"
     channel_id = channel_id.replace("-", "_")
+
     data = get_ranking(channel_id)
     string = ""
     counter = 1
@@ -185,19 +206,14 @@ async def top(query=None):
         string += f"{str(counter)}.{str(x[1])}: {str(x[4])}.  "
         counter += 1
     if len(string) > 200:
-        # split the string into parts of 200 characters and send it to request_url
-        parts = [string[i : i + 200] for i in range(0, len(string), 200)]
-        # yield parts[0]
-        await asyncio.sleep(5)
-        for part in parts:
-            requests.post(response_url, data={"message": part})
-            # if its not the second last part, wait for 5 seconds
-            if part != parts[-2]:
-                await asyncio.sleep(5)
-            else:
-                return parts[-1]
-        return "Here ^^"
-    return string
+        try:
+            return "Sorry, for spam!"
+        finally:
+            parts = [string[i : i + 200] for i in range(0, len(string), 200)]
+            for part in parts:
+                requests.post(response_url, data={"message": part})
+    else:
+        return string
 
 
 @app.get("/wordcount/")
@@ -207,7 +223,6 @@ def wordcount(word: str = None):
         return "Please pass a word(s) to count for."
     try:
         channel = parse_qs(request.headers["Nightbot-Channel"])
-        response_url = request.headers["Nightbot-Response-Url"]
     except KeyError:
         return "Not able to auth"
 
